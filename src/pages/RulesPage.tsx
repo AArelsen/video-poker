29c827f10b45a2ffa54180fe22b401bca2ee5cf2
AdamArelsen <adamarelsen@gmail.com>
2026-09-14 23:45:52 +0200

    feat: Lagt til application routing og navigation

---
+++ src/pages/RulesPage.tsx
@@ -0,0 +1,8 @@
+export function RulesPage(){
+    return(
+        <main>
+            <h1>Rulers and payouts</h1>
+            <p>The game rules and payout table will be displayed here.</p>
+        </main>
+    );
+}
